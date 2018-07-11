46537aab718cd0e66ece50ad15d3bd37914597ce
Loïs <boubaultlois@gmail.com>
2018-07-11 22:59:52 +0200

    add config model

--- src/hue/Config.cpp
+++ src/hue/Config.cpp
@@ -1,2 +1,114 @@
 
 #include "Config.hpp"
+
+const std::string &Config::getName() const {
+    return name;
+}
+
+void Config::setName(const std::string &name) {
+    Config::name = name;
+}
+
+const std::string &Config::getMac() const {
+    return mac;
+}
+
+void Config::setMac(const std::string &mac) {
+    Config::mac = mac;
+}
+
+bool Config::isDhcp() const {
+    return dhcp;
+}
+
+void Config::setDhcp(bool dhcp) {
+    Config::dhcp = dhcp;
+}
+
+const std::string &Config::getIpaddress() const {
+    return ipaddress;
+}
+
+void Config::setIpaddress(const std::string &ipaddress) {
+    Config::ipaddress = ipaddress;
+}
+
+const std::string &Config::getNetmask() const {
+    return netmask;
+}
+
+void Config::setNetmask(const std::string &netmask) {
+    Config::netmask = netmask;
+}
+
+const std::string &Config::getGateway() const {
+    return gateway;
+}
+
+void Config::setGateway(const std::string &gateway) {
+    Config::gateway = gateway;
+}
+
+const std::string &Config::getProxyaddress() const {
+    return proxyaddress;
+}
+
+void Config::setProxyaddress(const std::string &proxyaddress) {
+    Config::proxyaddress = proxyaddress;
+}
+
+int Config::getProxyport() const {
+    return proxyport;
+}
+
+void Config::setProxyport(int proxyport) {
+    Config::proxyport = proxyport;
+}
+
+const std::string &Config::getUTC() const {
+    return UTC;
+}
+
+void Config::setUTC(const std::string &UTC) {
+    Config::UTC = UTC;
+}
+
+const std::map<std::string, whitelistItem> &Config::getWhitelist() const {
+    return whitelist;
+}
+
+void Config::setWhitelist(const std::map<std::string, whitelistItem> &whitelist) {
+    Config::whitelist = whitelist;
+}
+
+const std::string &Config::getSwversion() const {
+    return swversion;
+}
+
+void Config::setSwversion(const std::string &swversion) {
+    Config::swversion = swversion;
+}
+
+const swupdateStruct &Config::getSwudpate() const {
+    return swudpate;
+}
+
+void Config::setSwudpate(const swupdateStruct &swudpate) {
+    Config::swudpate = swudpate;
+}
+
+bool Config::isLinkbutton() const {
+    return linkbutton;
+}
+
+void Config::setLinkbutton(bool linkbutton) {
+    Config::linkbutton = linkbutton;
+}
+
+bool Config::isPortalservices() const {
+    return portalservices;
+}
+
+void Config::setPortalservices(bool portalservices) {
+    Config::portalservices = portalservices;
+}
--- src/hue/Config.hpp
+++ src/hue/Config.hpp
@@ -2,9 +2,94 @@
 #ifndef HUEVITA_CONFIG_HPP
 #define HUEVITA_CONFIG_HPP
 
+#include <string>
+#include <map>
+
+struct whitelistItem {
+    std::string last_use_date;
+    std::string create_date;
+    std::string name;
+};
+
+struct swupdateStruct {
+    int         updatestate;
+    std::string url;
+    std::string text;
+    bool        notify;
+};
 
 class Config {
+private:
+    std::string name;
+    std::string mac;
+    bool        dhcp;
+    std::string ipaddress;
+    std::string netmask;
+    std::string gateway;
+    std::string proxyaddress;
+    int         proxyport;
+    std::string UTC;
+    std::map<std::string, whitelistItem> whitelist;
+    std::string swversion;
+    swupdateStruct    swudpate;
+    bool        linkbutton;
+    bool        portalservices;
+public:
+    const std::string &getName() const;
 
+    void setName(const std::string &name);
+
+    const std::string &getMac() const;
+
+    void setMac(const std::string &mac);
+
+    bool isDhcp() const;
+
+    void setDhcp(bool dhcp);
+
+    const std::string &getIpaddress() const;
+
+    void setIpaddress(const std::string &ipaddress);
+
+    const std::string &getNetmask() const;
+
+    void setNetmask(const std::string &netmask);
+
+    const std::string &getGateway() const;
+
+    void setGateway(const std::string &gateway);
+
+    const std::string &getProxyaddress() const;
+
+    void setProxyaddress(const std::string &proxyaddress);
+
+    int getProxyport() const;
+
+    void setProxyport(int proxyport);
+
+    const std::string &getUTC() const;
+
+    void setUTC(const std::string &UTC);
+
+    const std::map<std::string, whitelistItem> &getWhitelist() const;
+
+    void setWhitelist(const std::map<std::string, whitelistItem> &whitelist);
+
+    const std::string &getSwversion() const;
+
+    void setSwversion(const std::string &swversion);
+
+    const swupdateStruct &getSwudpate() const;
+
+    void setSwudpate(const swupdateStruct &swudpate);
+
+    bool isLinkbutton() const;
+
+    void setLinkbutton(bool linkbutton);
+
+    bool isPortalservices() const;
+
+    void setPortalservices(bool portalservices);
 };
 
 
