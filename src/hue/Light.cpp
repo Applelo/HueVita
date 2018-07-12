9a798cbde341cf7f614c6692b7d17fe2fe01709f
Loïs <boubaultlois@gmail.com>
2018-07-12 22:16:16 +0200

    add hue light model

--- src/hue/Light.cpp
+++ src/hue/Light.cpp
@@ -1,2 +1,50 @@
 
 #include "Light.hpp"
+
+const stateStruct &Light::getState() const {
+    return state;
+}
+
+void Light::setState(const stateStruct &state) {
+    Light::state = state;
+}
+
+const std::string &Light::getType() const {
+    return type;
+}
+
+void Light::setType(const std::string &type) {
+    Light::type = type;
+}
+
+const std::string &Light::getName() const {
+    return name;
+}
+
+void Light::setName(const std::string &name) {
+    Light::name = name;
+}
+
+const std::string &Light::getModelid() const {
+    return modelid;
+}
+
+void Light::setModelid(const std::string &modelid) {
+    Light::modelid = modelid;
+}
+
+const std::string &Light::getSwversion() const {
+    return swversion;
+}
+
+void Light::setSwversion(const std::string &swversion) {
+    Light::swversion = swversion;
+}
+
+const std::map<std::string, std::string> &Light::getPointsymbol() const {
+    return pointsymbol;
+}
+
+void Light::setPointsymbol(const std::map<std::string, std::string> &pointsymbol) {
+    Light::pointsymbol = pointsymbol;
+}
--- src/hue/Light.hpp
+++ src/hue/Light.hpp
@@ -2,8 +2,53 @@
 #ifndef HUEVITA_LIGHT_HPP
 #define HUEVITA_LIGHT_HPP
 
+#include <string>
+#include <map>
+
+struct stateStruct {
+    bool on;
+    int bri;
+    int sat;
+    int *xy;
+    int ct;
+    std::string alert;
+    std::string effect;
+    std::string colormode;
+    bool reachable;
+};
 
 class Light {
+    private:
+        stateStruct state;
+        std::string type;
+        std::string name;
+        std::string modelid;
+        std::string swversion;
+        std::map<std::string,std::string> pointsymbol;
+    public:
+        const stateStruct &getState() const;
+
+        void setState(const stateStruct &state);
+
+        const std::string &getType() const;
+
+        void setType(const std::string &type);
+
+        const std::string &getName() const;
+
+        void setName(const std::string &name);
+
+        const std::string &getModelid() const;
+
+        void setModelid(const std::string &modelid);
+
+        const std::string &getSwversion() const;
+
+        void setSwversion(const std::string &swversion);
+
+        const std::map<std::string, std::string> &getPointsymbol() const;
+
+        void setPointsymbol(const std::map<std::string, std::string> &pointsymbol);
 
 };
 
