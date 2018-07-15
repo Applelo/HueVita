d39e31a6d111948110522ca1e56514036ce256e8
Loïs <boubaultlois@gmail.com>
2018-07-15 21:06:05 +0200

    add hue sensor model (with json constructor)

--- src/hue/Sensor.cpp
+++ src/hue/Sensor.cpp
@@ -1,2 +1,63 @@
 
 #include "Sensor.hpp"
+
+Sensor::Sensor(json_t *json) {
+    this->config.on = json_boolean_value(json_object_get(json_object_get(json, "config"), "on"));
+    this->config.configured = json_boolean_value(json_object_get(json_object_get(json, "config"), "configured"));
+    this->config.sunriseoffset = (int) json_integer_value(json_object_get(json_object_get(json, "config"), "sunreiseoffeset"));
+    this->config.sunsetoffeset = (int) json_integer_value(json_object_get(json_object_get(json, "config"), "sunsetoffeset"));
+
+    this->name = json_string_value(json_object_get(json, "name"));
+    this->type = json_string_value(json_object_get(json, "type"));
+    this->modelid = json_string_value(json_object_get(json, "modelid"));
+    this->manufacturername = json_string_value(json_object_get(json, "manufacturername"));
+    this->swversion = json_string_value(json_object_get(json, "swversion"));
+}
+
+const configStruct &Sensor::getConfig() const {
+    return config;
+}
+
+void Sensor::setConfig(const configStruct &config) {
+    Sensor::config = config;
+}
+
+const std::string &Sensor::getName() const {
+    return name;
+}
+
+void Sensor::setName(const std::string &name) {
+    Sensor::name = name;
+}
+
+const std::string &Sensor::getType() const {
+    return type;
+}
+
+void Sensor::setType(const std::string &type) {
+    Sensor::type = type;
+}
+
+const std::string &Sensor::getModelid() const {
+    return modelid;
+}
+
+void Sensor::setModelid(const std::string &modelid) {
+    Sensor::modelid = modelid;
+}
+
+const std::string &Sensor::getManufacturername() const {
+    return manufacturername;
+}
+
+void Sensor::setManufacturername(const std::string &manufacturername) {
+    Sensor::manufacturername = manufacturername;
+}
+
+const std::string &Sensor::getSwversion() const {
+    return swversion;
+}
+
+void Sensor::setSwversion(const std::string &swversion) {
+    Sensor::swversion = swversion;
+}
--- src/hue/Sensor.hpp
+++ src/hue/Sensor.hpp
@@ -2,9 +2,50 @@
 #ifndef HUEVITA_SENSOR_HPP
 #define HUEVITA_SENSOR_HPP
 
+#include <string>
+#include <jansson.h>
+
+struct configStruct {
+    bool on;
+    bool configured;
+    int sunriseoffset;
+    int sunsetoffeset;
+};
 
 class Sensor {
+    private:
+        configStruct config;
+        std::string name;
+        std::string type;
+        std::string modelid;
+        std::string manufacturername;
+        std::string swversion;
+    public:
+        Sensor(json_t *json);
 
+    const configStruct &getConfig() const;
+
+    void setConfig(const configStruct &config);
+
+    const std::string &getName() const;
+
+    void setName(const std::string &name);
+
+    const std::string &getType() const;
+
+    void setType(const std::string &type);
+
+    const std::string &getModelid() const;
+
+    void setModelid(const std::string &modelid);
+
+    const std::string &getManufacturername() const;
+
+    void setManufacturername(const std::string &manufacturername);
+
+    const std::string &getSwversion() const;
+
+    void setSwversion(const std::string &swversion);
 };
 
 
