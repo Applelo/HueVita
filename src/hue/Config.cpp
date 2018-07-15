58f12e9e23c5f6d45332548e93432d7eae51f836
Loïs <boubaultlois@gmail.com>
2018-07-15 19:36:32 +0200

    update Hue config with last hue api

--- src/hue/Config.cpp
+++ src/hue/Config.cpp
@@ -4,6 +4,8 @@
 Config::Config(json_t *json)  {
 
     this->name  =   json_string_value(json_object_get(json, "name"));
+    this->zigbeechannel = (int) json_integer_value(json_object_get(json, "zigbeechannel"));
+    this->bridgeid = json_string_value(json_object_get(json, "bridgeid"));
     this->mac   =   json_string_value(json_object_get(json, "mac"));
     this->dhcp  =   json_boolean_value(json_object_get(json, "dhcp"));
     this->ipaddress = json_string_value(json_object_get(json, "ipaddress"));
@@ -12,6 +14,37 @@ Config::Config(json_t *json)  {
     this->proxyaddress = json_string_value(json_object_get(json, "proxyaddress"));
     this->proxyport = (int)json_integer_value(json_object_get(json, "gateway"));
     this->UTC = json_string_value(json_object_get(json, "UTC"));
+    this->localtime = json_string_value(json_object_get(json, "localtime"));
+    this->timezone = json_string_value(json_object_get(json, "timezone"));
+    this->modelid =  json_string_value(json_object_get(json, "modelid"));
+    this->datastoreversion =  json_string_value(json_object_get(json, "datastoreversion"));
+    this->swversion = json_string_value(json_object_get(json, "swversion"));
+    this->apiversion = json_string_value(json_object_get(json, "apiversion"));
+
+    this->swudpate.updatestate = (int) json_integer_value(json_object_get(json_object_get(json, "swupdate"), "updatestate"));
+    this->swudpate.url = json_string_value(json_object_get(json_object_get(json, "swupdate"), "url"));
+    this->swudpate.text = json_string_value(json_object_get(json_object_get(json, "swupdate"), "text"));
+    this->swudpate.notify = json_boolean_value(json_object_get(json_object_get(json, "swupdate"), "notify"));
+
+    this->swupdate2.checkforupdate = json_boolean_value(json_object_get(json_object_get(json, "swupdate2"), "checkforupdate"));
+    this->swupdate2.lastchange = json_string_value(json_object_get(json_object_get(json, "swupdate2"), "lastchange"));
+    this->swupdate2.state = json_string_value(json_object_get(json_object_get(json, "swupdate2"), "state"));
+
+    this->linkbutton = json_boolean_value(json_object_get(json, "linkbutton"));
+    this->portalservices = json_boolean_value(json_object_get(json, "portalservices"));
+    this->portalconnection = json_string_value(json_object_get(json, "portalconnection"));
+
+    this->portalstate.signedon = json_boolean_value(json_object_get(json_object_get(json, "portalstate"), "signedon"));
+    this->portalstate.incoming = json_boolean_value(json_object_get(json_object_get(json, "portalstate"), "incoming"));
+    this->portalstate.outgoing = json_boolean_value(json_object_get(json_object_get(json, "portalstate"), "outgoing"));
+    this->portalstate.communication = json_string_value(json_object_get(json_object_get(json, "portalstate"), "communication"));
+
+    this->internetservices.internet = json_string_value(json_object_get(json_object_get(json, "internetservices"), "internet"));
+    this->internetservices.remoteaccess = json_string_value(json_object_get(json_object_get(json, "internetservices"), "remoteaccess"));
+    this->internetservices.time = json_string_value(json_object_get(json_object_get(json, "internetservices"), "time"));
+    this->internetservices.swupdate = json_string_value(json_object_get(json_object_get(json, "internetservices"), "swupdate"));
+
+    this->factorynew =  json_boolean_value(json_object_get(json, "factorynew"));
 
     json_t *whitelist = json_object_get(json, "whitelist");
     const char *key;
@@ -23,16 +56,6 @@ Config::Config(json_t *json)  {
         item.name = json_string_value(json_object_get(value, "name"));
         this->whitelist.insert(std::pair<std::string,whitelistItem>(key, item));
     }
-
-    this->swversion = json_string_value(json_object_get(json, "swversion"));
-
-    this->swudpate.updatestate = (int) json_integer_value(json_object_get(json_object_get(json, "swupdate"), "updatestate"));
-    this->swudpate.url = json_string_value(json_object_get(json_object_get(json, "swupdate"), "url"));
-    this->swudpate.text = json_string_value(json_object_get(json_object_get(json, "swupdate"), "text"));
-    this->swudpate.notify = json_boolean_value(json_object_get(json_object_get(json, "swupdate"), "notify"));
-
-    this->linkbutton = json_boolean_value(json_object_get(json, "linkbutton"));
-    this->portalservices = json_boolean_value(json_object_get(json, "portalservices"));
 }
 
 const std::string &Config::getName() const {
@@ -41,6 +64,22 @@ const std::string &Config::getName() const {
 
 void Config::setName(const std::string &name) {
     Config::name = name;
+}
+
+int Config::getZigbeechannel() const {
+    return zigbeechannel;
+}
+
+void Config::setZigbeechannel(int zigbeechannel) {
+    Config::zigbeechannel = zigbeechannel;
+}
+
+const std::string &Config::getBridgeid() const {
+    return bridgeid;
+}
+
+void Config::setBridgeid(const std::string &bridgeid) {
+    Config::bridgeid = bridgeid;
 }
 
 const std::string &Config::getMac() const {
@@ -107,12 +146,36 @@ void Config::setUTC(const std::string &UTC) {
     Config::UTC = UTC;
 }
 
-const std::map<std::string, whitelistItem> &Config::getWhitelist() const {
-    return whitelist;
+const std::string &Config::getLocaltime() const {
+    return localtime;
 }
 
-void Config::setWhitelist(const std::map<std::string, whitelistItem> &whitelist) {
-    Config::whitelist = whitelist;
+void Config::setLocaltime(const std::string &localtime) {
+    Config::localtime = localtime;
+}
+
+const std::string &Config::getTimezone() const {
+    return timezone;
+}
+
+void Config::setTimezone(const std::string &timezone) {
+    Config::timezone = timezone;
+}
+
+const std::string &Config::getModelid() const {
+    return modelid;
+}
+
+void Config::setModelid(const std::string &modelid) {
+    Config::modelid = modelid;
+}
+
+const std::string &Config::getDatastoreversion() const {
+    return datastoreversion;
+}
+
+void Config::setDatastoreversion(const std::string &datastoreversion) {
+    Config::datastoreversion = datastoreversion;
 }
 
 const std::string &Config::getSwversion() const {
@@ -123,12 +186,28 @@ void Config::setSwversion(const std::string &swversion) {
     Config::swversion = swversion;
 }
 
+const std::string &Config::getApiversion() const {
+    return apiversion;
+}
+
+void Config::setApiversion(const std::string &apiversion) {
+    Config::apiversion = apiversion;
+}
+
 const swupdateStruct &Config::getSwudpate() const {
     return swudpate;
 }
 
 void Config::setSwudpate(const swupdateStruct &swudpate) {
     Config::swudpate = swudpate;
+}
+
+const swupdate2Struct &Config::getSwupdate2() const {
+    return swupdate2;
+}
+
+void Config::setSwupdate2(const swupdate2Struct &swupdate2) {
+    Config::swupdate2 = swupdate2;
 }
 
 bool Config::isLinkbutton() const {
@@ -145,5 +224,45 @@ bool Config::isPortalservices() const {
 
 void Config::setPortalservices(bool portalservices) {
     Config::portalservices = portalservices;
+}
+
+const std::string &Config::getPortalconnection() const {
+    return portalconnection;
+}
+
+void Config::setPortalconnection(const std::string &portalconnection) {
+    Config::portalconnection = portalconnection;
+}
+
+const portalstateStruct &Config::getPortalstate() const {
+    return portalstate;
+}
+
+void Config::setPortalstate(const portalstateStruct &portalstate) {
+    Config::portalstate = portalstate;
+}
+
+const internetservicesStruct &Config::getInternetservices() const {
+    return internetservices;
+}
+
+void Config::setInternetservices(const internetservicesStruct &internetservices) {
+    Config::internetservices = internetservices;
+}
+
+bool Config::isFactorynew() const {
+    return factorynew;
+}
+
+void Config::setFactorynew(bool factorynew) {
+    Config::factorynew = factorynew;
+}
+
+const std::map<std::string, whitelistItem> &Config::getWhitelist() const {
+    return whitelist;
+}
+
+void Config::setWhitelist(const std::map<std::string, whitelistItem> &whitelist) {
+    Config::whitelist = whitelist;
 }
 
--- src/hue/Config.hpp
+++ src/hue/Config.hpp
@@ -14,34 +14,75 @@ struct whitelistItem {
 
 struct swupdateStruct {
     int         updatestate;
+    bool        checkforupdate;
     std::string url;
     std::string text;
     bool        notify;
 };
 
+struct swupdate2Struct {
+    bool checkforupdate;
+    std::string lastchange;
+    std::string state;
+};
+
+struct portalstateStruct {
+    bool        signedon;
+    bool        incoming;
+    bool        outgoing;
+    std::string communication;
+};
+
+struct internetservicesStruct {
+    std::string internet;
+    std::string remoteaccess;
+    std::string time;
+    std::string swupdate;
+};
+
+
 class Config {
 private:
-    std::string name;
-    std::string mac;
-    bool        dhcp;
-    std::string ipaddress;
-    std::string netmask;
-    std::string gateway;
-    std::string proxyaddress;
-    int         proxyport;
-    std::string UTC;
+    std::string     name;
+    int             zigbeechannel;
+    std::string     bridgeid;
+    std::string     mac;
+    bool            dhcp;
+    std::string     ipaddress;
+    std::string     netmask;
+    std::string     gateway;
+    std::string     proxyaddress;
+    int             proxyport;
+    std::string     UTC;
+    std::string     localtime;
+    std::string     timezone;
+    std::string     modelid;
+    std::string     datastoreversion;
+    std::string     swversion;
+    std::string     apiversion;
+    swupdateStruct  swudpate;
+    swupdate2Struct swupdate2;
+    bool            linkbutton;
+    bool            portalservices;
+    std::string     portalconnection;
+    portalstateStruct       portalstate;
+    internetservicesStruct  internetservices;
+    bool            factorynew;
     std::map<std::string, whitelistItem> whitelist;
-    std::string swversion;
-    swupdateStruct    swudpate;
-    bool        linkbutton;
-    bool        portalservices;
 public:
     Config(json_t *json);
-
 
     const std::string &getName() const;
 
     void setName(const std::string &name);
+
+    int getZigbeechannel() const;
+
+    void setZigbeechannel(int zigbeechannel);
+
+    const std::string &getBridgeid() const;
+
+    void setBridgeid(const std::string &bridgeid);
 
     const std::string &getMac() const;
 
@@ -75,17 +116,37 @@ public:
 
     void setUTC(const std::string &UTC);
 
-    const std::map<std::string, whitelistItem> &getWhitelist() const;
+    const std::string &getLocaltime() const;
 
-    void setWhitelist(const std::map<std::string, whitelistItem> &whitelist);
+    void setLocaltime(const std::string &localtime);
+
+    const std::string &getTimezone() const;
+
+    void setTimezone(const std::string &timezone);
+
+    const std::string &getModelid() const;
+
+    void setModelid(const std::string &modelid);
+
+    const std::string &getDatastoreversion() const;
+
+    void setDatastoreversion(const std::string &datastoreversion);
 
     const std::string &getSwversion() const;
 
     void setSwversion(const std::string &swversion);
 
+    const std::string &getApiversion() const;
+
+    void setApiversion(const std::string &apiversion);
+
     const swupdateStruct &getSwudpate() const;
 
     void setSwudpate(const swupdateStruct &swudpate);
+
+    const swupdate2Struct &getSwupdate2() const;
+
+    void setSwupdate2(const swupdate2Struct &swupdate2);
 
     bool isLinkbutton() const;
 
@@ -94,6 +155,27 @@ public:
     bool isPortalservices() const;
 
     void setPortalservices(bool portalservices);
+
+    const std::string &getPortalconnection() const;
+
+    void setPortalconnection(const std::string &portalconnection);
+
+    const portalstateStruct &getPortalstate() const;
+
+    void setPortalstate(const portalstateStruct &portalstate);
+
+    const internetservicesStruct &getInternetservices() const;
+
+    void setInternetservices(const internetservicesStruct &internetservices);
+
+    bool isFactorynew() const;
+
+    void setFactorynew(bool factorynew);
+
+    const std::map<std::string, whitelistItem> &getWhitelist() const;
+
+    void setWhitelist(const std::map<std::string, whitelistItem> &whitelist);
+
 };
 
 
